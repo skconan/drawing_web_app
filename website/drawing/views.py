3289acc42264e8d870f8a42417cbe246c29680b2
skconan <supakit.kr@gmail.com>
2018-11-28 09:57:30 +0700

    finsih (can use not performance)

--- website/drawing/views.py
+++ website/drawing/views.py
@@ -5,15 +5,26 @@ from image_processing.image_processing import *
 from image_processing.models import Image
 import random
 
+
 # Create your views here.
 def masker(req):
+    context = {}
     template = 'masker.html'
-    img_name = '1543344041.jpg';
-    img_url = 'http://127.0.0.1:8000/media/dataset/images/'+img_name
-    i = Image.objects.filter(mask=False)
-    index = random.randint(0,len(i)-1)
-    image_name = i[index].name
-    context = {'img_name':image_name}
+    if req.method == 'POST':
+        print("POST")
+        image_data = req.POST['mask-result']
+        image_name = req.POST['image-name']
+        save_canvas(image_data,image_name)
+        i = Image.objects.get(name=image_name)
+        i.mask = True
+        i.save()
+    else:      
+        i = Image.objects.filter(mask=False)
+        index = random.randint(0,len(i)-1)
+        image_name = i[index].name
+        # img_url = 'http://127.0.0.1:8000/media/dataset/images/'+img_name+".jpg"
+        
+        context = {'img_name':image_name}
     return render(req, template, context)
 
 def upload(req):
--- website/image_processing/image_processing.py
+++ website/image_processing/image_processing.py
@@ -2,8 +2,14 @@ import cv2 as cv
 import time
 from .models import Image
 from website import settings
+from io import BytesIO
+from PIL import Image
+import re
+import base64
+import numpy as np
 
 PATH_IMG = settings.MEDIA_ROOT+"dataset/images/"
+PATH_GROUNDTRUTH = settings.MEDIA_ROOT+"dataset/groundTruth/"
 
 
 def record(image_name):
@@ -24,14 +30,25 @@ def video2img(file_name, sampling):
                 cap.release()
                 break
             continue
-        
+
         if count % sampling == 0 and ret:
             image_name = str(time.time())[:10]
             c = 484  # //1936 / 4
             r = 304  # //1216 / 4
-            img = cv.resize(frame,(c,r))
+            img = cv.resize(frame, (c, r))
             cv.imwrite(PATH_IMG + "\\" + image_name + ".jpg", img)
             record(image_name)
             count_false = 0
 
         count += 1
+
+def save_canvas(image_data,image_name):
+    im = canvas2img(image_data)
+    im.save(PATH_GROUNDTRUTH+image_name+".png", 'PNG')
+
+def canvas2img(image_data):
+    image_data = re.sub("^data:image/png;base64,", "", image_data)
+    image_data = base64.b64decode(image_data)
+    image_data = BytesIO(image_data)
+    im = Image.open(image_data)
+    return im
