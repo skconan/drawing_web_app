440b43d76fa519cd169a8b1d6c1a77da80cb36bc
skconan <supakit.kr@gmail.com>
2018-11-28 07:20:48 +0700

    fix media path and fix vdo2img

--- website/image_processing/image_processing.py
+++ website/image_processing/image_processing.py
@@ -1,30 +1,37 @@
 import cv2 as cv
 import time
 from .models import Image
+from website import settings
 
-PATH_IMG = r'C:\Users\skconan\Desktop\computer_vision\dataset\images'
+PATH_IMG = settings.MEDIA_ROOT+"dataset/images/"
+
 
 def record(image_name):
-    i = Image(image_name,False)
+    i = Image(image_name, False)
     i.save()
 
-def video2img(file_path,sampling):
-    file_path = r"C:\Users\skconan\Desktop\computer_vision\website\website\videos\\"+file_path
+
+def video2img(file_name, sampling):
+    file_path = settings.MEDIA_ROOT+"videos/"+file_name
     cap = cv.VideoCapture(file_path)
     count = 0
     count_false = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
-        print(ret)
+        if not ret:
+            count_false += 1
+            if count_false > 30:
+                cap.release()
+                break
+            continue
+        
         if count % sampling == 0 and ret:
-            image_name =  str(time.time())[:10]
-            cv.imwrite(PATH_IMG + "\\" + image_name + ".jpg",frame)
+            image_name = str(time.time())[:10]
+            c = 484  # //1936 / 4
+            r = 304  # //1216 / 4
+            img = cv.resize(frame,(c,r))
+            cv.imwrite(PATH_IMG + "\\" + image_name + ".jpg", img)
             record(image_name)
             count_false = 0
-        else:
-            count_false += 1
-        if count_false > 10:
-            cap.release()
-            break
-        count+=1
-        
+
+        count += 1
