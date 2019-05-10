c60e9a49ed58444c8460407b5b4c6e54da023a6d
skconan <supakit.kr@gmail.com>
2019-05-10 19:31:44 +0700

    fix static dir

--- website/website/settings.py
+++ website/website/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL= "/media/"
