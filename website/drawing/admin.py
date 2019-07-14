fa383004372408544ad18e511b4bf17216e3543d
skconan <supakit.kr@gmail.com>
2019-07-15 05:04:10 +0700

    Register mission

--- website/drawing/admin.py
+++ website/drawing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import VideoUpload
+from .models import VideoUpload, Mission
 
 # Register your models here.
 
 admin.site.register(VideoUpload)
+admin.site.register(Mission)
