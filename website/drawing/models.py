1a96e8aa1c32ddbdab8afb4eb133765173a220f3
skconan <supakit.kr@gmail.com>
2019-07-14 20:53:55 +0700

    add mission tb

--- website/drawing/models.py
+++ website/drawing/models.py
@@ -8,3 +8,8 @@ class VideoUpload(models.Model):
     video = models.FileField(upload_to="videos")
     def __str__(self):
         return self.name
+
+class Mission(models.Model):
+    name = models.CharField(primary_key=True,max_length=50)
+    color = models.CharField(max_length=10)
+    labeled = models.IntegerField()
